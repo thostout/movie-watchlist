eced6797009ad0b43fbf42345d07eaeb0a005cd5
thostout <thostout3@gmail.com>
2026-01-05 13:29:21 -0500

    Refactor project structure: remove main.py, add database.py, and update dependencies in pyproject.toml and uv.lock

--- main/watchlist.py
+++ main/watchlist.py
@@ -1,12 +1,12 @@
 import tmdbsimple as tmdb
 
-tmdb.API_KEY = '4f5ab13628b2c3705dc646b7673e3c7e'
+tmdb.API_KEY = "4f5ab13628b2c3705dc646b7673e3c7e"
 
 movie_name = input("What movie do you want to search for? ")
 
 search = tmdb.Search()
 response = search.movie(query=movie_name)
-results = response['results']
+results = response["results"]
 print(f"Found {len(results)} movies")
 
 
@@ -16,7 +16,7 @@ for i, movie in enumerate(results):
 choice = int(input("\nPick the number corelating to corect movie. "))
 
 selected_movie = results[choice]
-movie_id = selected_movie['id']
+movie_id = selected_movie["id"]
 
 movie = tmdb.Movies(movie_id)
 detail = movie.info()
